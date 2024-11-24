b509dec4ab189e3962e8596ce6360eea2ae7a10c
Graham Arnett <GrahamKentArnett@outlook.com>
2024-11-24 17:18:18 -0500

    init commit

---
+++ MidPointApp/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace MidPointApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, welcome to my midpoint formula calculator!");
+            string choice;
+            do
+            {
+                Console.Write("Enter x1: ");
+                double xOne = double.Parse(Console.ReadLine());
+                Console.Write("Enter x2: ");
+                double xTwo = double.Parse(Console.ReadLine());
+                Console.Write("Enter y1: ");
+                double yOne = double.Parse(Console.ReadLine());
+                Console.Write("Enter y2: ");
+                double yTwo = double.Parse(Console.ReadLine());
+
+                double midPointX = (xOne + xTwo)/2;
+                double midPointY = (yOne + yTwo)/2;
+
+                Console.WriteLine($"Your midpoint is ({midPointX} , {midPointY})");
+
+                Console.Write("Would you like to go again? (y/n): ");
+                choice = Console.ReadLine().ToLower();
+            } while (choice == "y");
+        }
+    }
+}
